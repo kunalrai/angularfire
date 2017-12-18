39e66ed21a1636a12d89ee72cc976775924f2e3f
Kunal Rai <ikunalrai@gmail.com>
2017-12-18 10:15:38 +0530

    some changes

--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,6 +1,6 @@
-import { browser, element, by } from 'protractor';
+import { browser, by, element } from 'protractor';
 
-export class ProplistingsPage {
+export class AppPage {
   navigateTo() {
     return browser.get('/');
   }
